5b670b7b2abdb979b4b4c3b2cda200b97b01e659
Javier Tejedor <javi.teje99@gmail.com>
2021-11-10 21:12:56 +0100

    refactored

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
 portfolioItems.forEach(portfolioItem => {
     portfolioItem.addEventListener('mouseover', () => {
         portfolioItem.childNodes[1].classList.add('img-darken');
-    })
+    });
     portfolioItem.addEventListener('mouseout', () => {
         portfolioItem.childNodes[1].classList.remove('img-darken');
-    })
-})
+    });
+});
 
-var brand = document.getElementById('brand');
 var page_title = document.getElementById('title');
 var titles = [
     'Javier Tejedor',
@@ -22,7 +21,7 @@ var titles = [
 
 function newTitle() {
     var i = (Math.random() * titles.length) | 0;
-    brand.innerText = titles[i];
+    page_title.innerText = titles[i];
 }
 
-newTitle()
+newTitle();
