7ea8236424456ef37ba3803099e8314e56798019
Javier Tejedor <javi.teje99@gmail.com>
2021-11-18 20:14:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
-portfolioItems.forEach(portfolioItem => {
-    portfolioItem.addEventListener('mouseover', () => {
-        portfolioItem.childNodes[1].classList.add('img-darken');
-    });
-    portfolioItem.addEventListener('mouseout', () => {
-        portfolioItem.childNodes[1].classList.remove('img-darken');
-    });
-});
-
 var page_title = document.getElementById('title');
 var titles = [
     'Javier Tejedor',
