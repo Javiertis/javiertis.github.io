3ed713a43b18b9c55d5d3d982dd4d1810dbfc913
Javier Tejedor <javi.teje99@gmail.com>
2021-11-19 17:57:25 +0100

    updated

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var pageTitles = Object.values(document.getElementsByClassName('title'));
-var titles = [
-    'Javier Tejedor',
-    'J4v13r T3j3d0r',
-    '/home/javier_tejedor/portfolio $',
-    'C:\\Users\\javier_tejedor >',
-    'Console.WriteLine("Javier Tejedor")',
-    'echo "Javier Tejedor"',
-    'System.out.println("Javier Tejedor")'
-];
-
-setInterval(() => {
+const switchTitle = () => {
+    const pageTitles = Object.values(document.getElementsByClassName('title'));
+    const titles = [
+        'Javier Tejedor',
+        'J4v13r T3j3d0r',
+        '/home/javier_tejedor/portfolio $',
+        'C:\\Users\\javier_tejedor >',
+        'Console.WriteLine("Javier Tejedor")',
+        'echo "Javier Tejedor"',
+        'System.out.println("Javier Tejedor")'
+    ];
     pageTitles.forEach(p => p.innerHTML = titles[(Math.random() * titles.length) | 0]);
-}, 60_000);
+};
+
+setInterval(switchTitle, 60_000);
+
+const switchDark = () => document.getElementsByTagName('body')[0].classList.toggle('dark');
